9ea2e728aba2c95242aedf5d32f27ecffb57a085
WaqasDogar <85635866+WaqasDogar@users.noreply.github.com>
2022-06-23 04:01:07 +0500

    Update urls.py

--- RoyalKitchen/RoyalKitchen/urls.py
+++ RoyalKitchen/RoyalKitchen/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
 
     path('',include('Myapp.urls')), 
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}), 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
