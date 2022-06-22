10abec0f052897d13651afd8dfe6ada70c9d534b
WaqasDogar <85635866+WaqasDogar@users.noreply.github.com>
2022-06-23 04:06:17 +0500

    Update urls.py

--- RoyalKitchen/RoyalKitchen/urls.py
+++ RoyalKitchen/RoyalKitchen/urls.py
@@ -18,8 +18,6 @@ from django.urls import path
 from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
